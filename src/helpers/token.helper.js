6018e0312445a433d4e0bf3bacf4f9a09613fff0
Afrosiyob <afrosiyob.a97@gmail.com>
2021-10-02 17:00:36 +0500

    add
    addd

--- src/helpers/token.helper.js
+++ src/helpers/token.helper.js
@@ -1,36 +1,36 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { v4: uuidv4 } = require("uuid");
-const { logger } = require("../logger/logger");
-const { Token } = require("../models/models");
+const jwt = require( "jsonwebtoken" );
+const config = require( "config" );
+const { v4: uuidv4 } = require( "uuid" );
+const { logger } = require( "../logger/logger" );
+const { Token } = require( "../models/models" );
 
-const generateAccessToken = (userId) => ({
-    accessToken: jwt.sign({ userId: userId, type: "access" },
-        config.get("jwtSecret"), { expiresIn: "1d" }
+const generateAccessToken = ( userId ) => ( {
+    accessToken: jwt.sign( { userId: userId, type: "access" },
+        config.get( "jwtSecret" ), { expiresIn: "3s" }
     ),
-});
+} );
 
 const generateRefreshToken = () => {
     const tokenId = uuidv4();
     return {
         tokenId: tokenId,
-        refreshToken: jwt.sign({ tokenId: tokenId, type: "refresh" },
-            config.get("jwtSecret"), { expiresIn: "30d" }
+        refreshToken: jwt.sign( { tokenId: tokenId, type: "refresh" },
+            config.get( "jwtSecret" ), { expiresIn: "30d" }
         ),
     };
 };
 
-const replaceFromDBRefreshToken = async (tokenId, userId) => {
+const replaceFromDBRefreshToken = async ( tokenId, userId ) => {
     try {
-        return await Token.findOne({ UserId: userId }, (err, token) => {
-            if (token) {
-                return Token.findByIdAndRemove(token.id, () => Token.create({ tokenId: tokenId, UserId: userId }))
+        return await Token.findOne( { UserId: userId }, ( err, token ) => {
+            if ( token ) {
+                return Token.findByIdAndRemove( token.id, () => Token.create( { tokenId: tokenId, UserId: userId } ) )
             } else {
-                return Token.create({ tokenId: tokenId, UserId: userId })
+                return Token.create( { tokenId: tokenId, UserId: userId } )
             }
-        })
-    } catch (error) {
-        logger.error(error);
+        } )
+    } catch ( error ) {
+        logger.error( error );
     }
 }
 
