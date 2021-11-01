8a3bb020089145229bd0d6591c99695e75ff3934
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-11-01 13:48:15 +0500

    add
    addd

--- server/server.js
+++ server/server.js
@@ -1,62 +1,79 @@
-const express = require( "express" );
-const winston = require( "winston" );
-const { errorHandler } = require( "../src/errors/ErrorHandler" );
-const morgan = require( "morgan" );
-const { logger } = require( "../src/logger/logger" );
-const path = require( "path" );
-const { userRouter } = require( "../src/routes/user.routes" );
-const mongoDb = require( "../connection/connectionMongo" );
-const { authRouter } = require( "../src/routes/auth.routes" );
-const { productRouter } = require( "../src/routes/product.routes" );
-const { indexRouter } = require( "../src/routes/index.routes" );
+const express = require("express");
+const winston = require("winston");
+const { errorHandler } = require("../src/errors/ErrorHandler");
+const morgan = require("morgan");
+const { logger } = require("../src/logger/logger");
+const path = require("path");
+const mongoDb = require("../connection/connectionMongo");
+const cors = require('cors')
 
-const swaggerUi = require( 'swagger-ui-express' );
-const swaggerDocument = require( '../doc/swagger.json' )
+const { userRouter } = require("../src/routes/user.routes");
+const { authRouter } = require("../src/routes/auth.routes");
+const { productRouter } = require("../src/routes/product.routes");
+const { indexRouter } = require("../src/routes/index.routes");
 
-const cors = require( 'cors' )
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../doc/swagger.json')
+const swaggerJsDoc = require('swagger-jsdoc')
+
 
 // require('dotenv').config()
 // create app server
 const app = express()
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "A simple Express Library API"
+        },
+        servers: [
+            {
+                url: "http://localhost:5000"
+            }
+        ],
+    },
+    apis: ['../src/*.routes.js'], // files containing annotations as above
+}
+
+const specs = swaggerJsDoc(options);
+
 // swagger doc
-app.use( '/api-docs', swaggerUi.serve, swaggerUi.setup( swaggerDocument ) );
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // cors
-app.use( cors() )
+app.use(cors())
 
 // set access json req.body
-app.use( express.json( { extended: true } ) );
-app.use( express.urlencoded( { extended: false } ) );
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // second way create static files
-app.use( "/public", express.static( path.join( __dirname, "public" ) ) );
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // ANCHOR show request to console only development
-if ( app.get( "env" ) === "development" ) {
-    app.use( morgan( "tiny" ) );
+if (app.get("env") === "development") {
+    app.use(morgan("tiny"));
     // Write log
     logger.add(
-        new winston.transports.Console( {
+        new winston.transports.Console({
             format: winston.format.simple(),
-        } )
+        })
     );
 }
 
 
 
-app.use( "/", indexRouter );
-app.use( "/api/user", userRouter );
-app.use( "/api/product", productRouter );
-app.use( "/api/auth", authRouter );
-
-
-
+app.use("/", indexRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/auth", authRouter);
 
 // ANCHOR error handler
-app.use( errorHandler );
+app.use(errorHandler);
 
 const { PORT = 5000, LOCAL_ADDRESS = '0.0.0.0' } = process.env
 
-
-app.listen( PORT, LOCAL_ADDRESS, mongoDb );
+app.listen(PORT, LOCAL_ADDRESS, mongoDb);
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,60 @@
-const { Router } = require( "express" );
-const { authLogin, authLogout, authMe, refreshTokens } = require( "../controller/auth.controller" );
-const { validationError, checkAuthToken } = require( "../middlewares/middlewares" );
-const { authLoginValidation } = require( "../validations/auth.validations" );
+const { Router } = require("express");
+const { authLogin, authLogout, authMe, refreshTokens } = require("../controller/auth.controller");
+const { validationError, checkAuthToken } = require("../middlewares/middlewares");
+const { authLoginValidation } = require("../validations/auth.validations");
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Login:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: username of user
+ *         password: 
+ *           type: string
+ *           description: password of user
+ *       example:
+ *           username: admin
+ *           password: 1234567
+ */
 
+/**
+ * GET /
+ * @summary Homepage
+ * @tags Login
+ *
+ * @return {object} 200 - success response
+ * @example response - 200 - success response
+ * {
+ *     "success": true,
+ * }
+ */
 
-router.post( "/login", authLoginValidation, validationError, authLogin );
-router.get( "/logout", checkAuthToken, authLogout );
-router.get( "/me", checkAuthToken, authMe );
-router.post( "/refresh_tokens", refreshTokens );
+/**
+ * @openapi
+ * /logout:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+
+router.post("/login", authLoginValidation, validationError, authLogin);
+
+router.get("/logout", checkAuthToken, authLogout);
+
+router.get("/me", checkAuthToken, authMe);
+
+router.post("/refresh_tokens", refreshTokens);
 
 module.exports = {
     authRouter: router,
